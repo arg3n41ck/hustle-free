c995f3f7f963e4477c215ab16cea3cce0aae5c5e
Argo-mafioznik <argenalimbaev@gmail.com>
2022-04-21 18:15:22 +0600

    not worked backgorund image in mainpagewithauth

--- src/components/pages/MainPageForNotAuthUser/HeroMainPageForNotAuth.js
+++ src/components/pages/MainPageForNotAuthUser/HeroMainPageForNotAuth.js
@@ -1,6 +1,5 @@
 import React from "react"
 import styled from "styled-components"
-import { Container } from "../../ui/Wrappers/Container"
 import twoDude from "../../../public/jpg/twodude.jpg"
 import Image from "next/image"
 
