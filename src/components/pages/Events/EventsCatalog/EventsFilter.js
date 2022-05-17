59ac254f4ac528fc74eaf68da2d0854581cab5d5
adylbek <aaaadyl.erg@gmail.com>
2022-05-17 10:31:18 +0600

    events filtration by date start & end

--- src/components/pages/Events/EventsCatalog/EventsFilter.js
+++ src/components/pages/Events/EventsCatalog/EventsFilter.js
@@ -14,19 +14,10 @@ import {
 import useQuery from "../../../../hooks/useQuery"
 import { useRouter } from "next/router"
 import { fetchEventsByParams } from "../../../../redux/components/events"
-
-const dateTypes = [
-  {
-    id: "dateType1",
-    name: "По возрастанию",
-    value: "date_start",
-  },
-  {
-    id: "dateType2",
-    name: "По убыванию",
-    value: "-date_start",
-  },
-]
+import { LocalizationProvider, MobileDatePicker } from "@mui/lab"
+import AdapterDateFns from "@mui/lab/AdapterDateFns"
+import { ru } from "date-fns/locale"
+import { format } from "date-fns"
 
 function EventsFilter() {
   const [isFiltersOpen, setFilter] = useState(false)
@@ -53,6 +44,14 @@ function EventsFilter() {
     [query]
   )
 
+  const handleDateRange = useCallback(
+    (name, value) => {
+      value ? query.set(name, value) : query.delete(name)
+      routerPush(`/events/?${query}`)
+    },
+    [query]
+  )
+
   useEffect(() => {
     dispatch(fetchEventsByParams(query))
   }, [query])
@@ -65,9 +64,8 @@ function EventsFilter() {
     countries.length &&
     countries.find((type) => type.name === query.get("country"))
 
-  const orderingValue =
-    query.get("ordering") &&
-    dateTypes.find((type) => type.value === query.get("ordering"))
+  const fromDate = query.get("date_start") || null
+  const toDate = query.get("date_end") || null
 
   const futureValue =
     query.get("events_type_time") === "future" ? null : { name: "future" }
@@ -147,25 +145,56 @@ function EventsFilter() {
               )}
             />
           )}
-          <Autocomplete
-            noOptionsText={"Ничего не найдено"}
-            onChange={(e, value) => handleFilter("ordering", value)}
-            options={dateTypes.map((option) => option)}
-            getOptionLabel={(option) => option.name}
-            value={orderingValue}
-            fullWidth
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                fullWidth
-                placeholder="Даты"
-                InputProps={{
-                  ...params.InputProps,
-                  startAdornment: <CalendarIcon />,
-                }}
-              />
-            )}
-          />
+          <LocalizationProvider locale={ru} dateAdapter={AdapterDateFns}>
+            <MobileDatePicker
+              toolbarTitle={"Выбрать дату от"}
+              cancelText={"Отмена"}
+              value={fromDate}
+              onChange={(value) => {
+                handleDateRange(
+                  "date_start",
+                  value && format(value, "yyyy-MM-dd")
+                )
+              }}
+              clearable
+              clearText="Убрать"
+              inputFormat="dd/MM/yyyy"
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  fullWidth
+                  inputProps={{
+                    ...params.inputProps,
+                    placeholder: "Дата от",
+                  }}
+                />
+              )}
+            />
+            <MobileDatePicker
+              toolbarTitle={"Выбрать дату до"}
+              cancelText={"Отмена"}
+              value={toDate}
+              onChange={(value) => {
+                handleDateRange(
+                  "date_end",
+                  value && format(value, "yyyy-MM-dd")
+                )
+              }}
+              clearable
+              clearText="Убрать"
+              inputFormat="dd/MM/yyyy"
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  fullWidth
+                  inputProps={{
+                    ...params.inputProps,
+                    placeholder: "Дата до",
+                  }}
+                />
+              )}
+            />
+          </LocalizationProvider>
           <Autocomplete
             noOptionsText={"Ничего не найдено"}
             onChange={(e, value) => sortHandler(e, value)}
@@ -373,37 +402,6 @@ export const LocationIcon = () => (
       clipRule="evenodd"
       d="M12.398 19.804C13.881 19.0348 19 16.0163 19 11C19 7.13401 15.866 4 12 4C8.13401 4 5 7.13401 5 11C5 16.0163 10.119 19.0348 11.602 19.804C11.8548 19.9351 12.1452 19.9351 12.398 19.804ZM12 14C13.6569 14 15 12.6569 15 11C15 9.34315 13.6569 8 12 8C10.3431 8 9 9.34315 9 11C9 12.6569 10.3431 14 12 14Z"
       fill="#828282"
-    />
-  </svg>
-)
-
-const CalendarIcon = () => (
-  <svg
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <rect
-      x="3"
-      y="6"
-      width="18"
-      height="15"
-      rx="2"
-      stroke="#828282"
-      strokeWidth="2"
-    />
-    <path
-      d="M3 10C3 8.11438 3 7.17157 3.58579 6.58579C4.17157 6 5.11438 6 7 6H17C18.8856 6 19.8284 6 20.4142 6.58579C21 7.17157 21 8.11438 21 10H3Z"
-      fill="#828282"
-    />
-    <path d="M7 3L7 6" stroke="#828282" strokeWidth="2" strokeLinecap="round" />
-    <path
-      d="M17 3L17 6"
-      stroke="#828282"
-      strokeWidth="2"
-      strokeLinecap="round"
     />
   </svg>
 )
